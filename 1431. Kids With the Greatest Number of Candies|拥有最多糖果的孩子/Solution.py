19cb8cc683d4866b979636c241c1b7a7c6ab559b
adamqddnh <adamqddnh@163.com>
2020-06-01 14:59:15 +0800

    Create Solution.py

---
+++ 1431. Kids With the Greatest Number of Candies|拥有最多糖果的孩子/Solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        maxCandy = max(candies)
+        return [candies[i] + extraCandies >= maxCandy for i in range(0, len(candies))]
