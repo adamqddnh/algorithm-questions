1e041da0cc758a83d46d76d9e66855d11884bafb
adamqddnh <adamqddnh@163.com>
2020-06-08 15:15:50 +0800

    Create Solution.py

---
+++ 231. Power of Two/Solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        while n >= 1:
+            if n == 1:
+                return True
+            if n % 2 == 1:
+                return False
+            n = n // 2
+        return False
