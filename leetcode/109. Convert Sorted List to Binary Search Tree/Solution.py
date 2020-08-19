b3ea599af168a43bb355e79045fe6b1552980bb4
adamqddnh <adamqddnh@163.com>
2020-08-19 14:32:02 +0800

    Create Solution.py

---
+++ leetcode/109. Convert Sorted List to Binary Search Tree/Solution.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        if not head:
+            return None
+        if not head.next:
+            return TreeNode(head.val)
+        pre, mid = self.findMid(head)
+        if pre:
+            pre.next = None
+        root = TreeNode(mid.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(mid.next)
+        return root
+
+    def findMid(self, head):
+        pre, slow, fast = None, head, head
+        while fast and fast.next:
+            pre = slow
+            slow = slow.next
+            fast = fast.next.next
+        return pre, slow
