e554f4940881f14ac64fedb9ee5ab40a2b716735
adamqddnh <adamqddnh@163.com>
2020-10-13 15:06:23 +0800

    Create Solution.py

---
+++ leetcode/24. Swap Nodes in Pairs/Solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        result.next = head
+        temp = result
+        while temp.next and temp.next.next:
+            node1 = temp.next
+            node2 = temp.next.next
+            temp.next = node2
+            node1.next = node2.next
+            node2.next = node1
+            temp = node1
+        return result.next
