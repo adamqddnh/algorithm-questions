18d57d0237a07e60a3d5128a5dd3103992826b7d
adamqddnh <adamqddnh@163.com>
2020-12-21 13:05:19 +0800

    Create Solution.py

---
+++ leetcode/746. Min Cost Climbing Stairs/Solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        if len(cost) < 2:
+            return 0
+        dp = [0] * len(cost)
+        dp[0], dp[1] = cost[0], cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+        return min(dp[-1], dp[-2])
