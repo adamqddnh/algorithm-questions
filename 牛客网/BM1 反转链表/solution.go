3b8a9ae010d915f244036fd9baf0710ec148e983
adamqddnh <adamqddnh@163.com>
2024-07-09 16:10:06 +0800

    Create solution.go

---
+++ 牛客网/BM1 反转链表/solution.go
@@ -0,0 +1,32 @@
+package main
+// import "fmt"
+import . "nc_tools"
+/*
+ * type ListNode struct{
+ *   Val int
+ *   Next *ListNode
+ * }
+ */
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 
+ * @param head ListNode类 
+ * @return ListNode类
+*/
+func ReverseList( head *ListNode ) *ListNode {
+    // write code here
+    var (
+        left *ListNode
+        right = head
+    )
+    
+    for head != nil {
+        right = head.Next
+        head.Next = left
+        left = head
+        head = right
+    }
+    return left
+}
