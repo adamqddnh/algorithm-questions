22e13dab9687c0822bbfee08e592ab1ed9dcd284
adamqddnh <adamqddnh@163.com>
2024-07-10 16:37:38 +0800

    Create solution.go

---
+++ 牛客网/BM12 单链表的排序/solution.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	. "nc_tools"
+	"sort"
+)
+
+/*
+ * type ListNode struct{
+ *   Val int
+ *   Next *ListNode
+ * }
+ */
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param head ListNode类 the head node
+ * @return ListNode类
+ */
+func sortInList( head *ListNode ) *ListNode {
+    // write code here
+    var (
+        arr = []int{}
+        result *ListNode
+    )
+    for head != nil {
+        arr = append(arr, head.Val)
+        head = head.Next
+    }
+    sort.Slice(arr, func(i, j int) bool {
+        if arr[i] >= arr[j] {
+            return true
+        }
+        return false
+    })
+
+    for i := 0; i < len(arr); i++ {
+        result = &ListNode{Val: arr[i], Next: result}
+    }
+    fmt.Println(arr)
+
+    return result
+}
